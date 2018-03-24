f57dba7f194366969bf2ba2e76d54583d311cf12
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2018-03-24 18:23:05 +0100

    updated requirements

--- bot.py
+++ bot.py
@@ -3,3 +3,15 @@
 import os
 import message
 
+from slackclient import SlackClient 
+
+authed_items = {}
+
+class Bot(object):
+
+    def __init__(self):
+        super(Bot, self).__init__()
+        self.name = "toto-boto"
+        self.emoji = ":fobot-face:"
+
+        self()
